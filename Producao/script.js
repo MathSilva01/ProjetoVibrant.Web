5b38d0169120e0913b2717c544aa7e80ebd81c05
Matheus Silva <107940346+MathSilva01@users.noreply.github.com>
2022-09-18 16:10:33 -0300

    Revert "Erro"
    This reverts commit dbf3324b3ea2411c0e521fb35e86d6dd5b149774.

--- Producao/script.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function(){
-   $(".iframe").colorbox({iframe:true, width:"100%", height:"100%"});
-   $.colorbox({href:"comunicado.php"});
-   $('#video_player').prop('autoplay', true);
-});
