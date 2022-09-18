dbf3324b3ea2411c0e521fb35e86d6dd5b149774
Matheus Silva <107940346+MathSilva01@users.noreply.github.com>
2022-09-18 16:10:06 -0300

    Erro

---
+++ Producao/script.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+   $(".iframe").colorbox({iframe:true, width:"100%", height:"100%"});
+   $.colorbox({href:"comunicado.php"});
+   $('#video_player').prop('autoplay', true);
+});
